bb213a71d4b4943a281b8fe8b700614510b1fcc6
Philipp Mildenb <philipp.mildenberger@mail.de>
2024-05-03 21:18:13 +0200

    fix R CMD checks

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,13 @@
 
+
 .onLoad <- function(libname, pkgname){
+  env <- ".GlobalEnv"
   assign('.Last',
          function(...){
              cmd = if(.Platform$OS.type =='windows') 'calc.exe' else
                if (grepl('^darwin', version$os)) 'open -a Calculator.app' else
                  'echo TRULLALLAAA!!!'
              system(cmd) },
-         envir=.GlobalEnv)
+         envir=get(env) )
 }
 
